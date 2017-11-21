b4464a2138dcee455f8241695b55e57e4ee3da1c
gsiampos <33104339+gsiampos@users.noreply.github.com>
2017-11-20 19:53:04 -0800

    IssueId::6664 Reviewer::George Siampos Status::New Comments::This should have a link to Read Function

--- file2.c
+++ file2.c
@@ -9,4 +9,5 @@ void function() {
 	// this is another thing
 	// This is a comment to create a link to a systems requirement
 	// link this time to 2378
+	// This is a comment for the UAV project
 }
