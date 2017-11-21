48ad57ec6b7db17fd5984a9c69ff550b5579d277
gsiampos <33104339+gsiampos@users.noreply.github.com>
2017-11-21 11:43:55 -0800

    IssueId::6664 Reviewer::George Siampos Status::New Comments::This was done to implement the read function at 6664

--- file.c
+++ file.c
@@ -12,4 +12,5 @@ void anotherfunction(){
 	//After a successful creation of a commit item.  Trouble shooting the link
 	//change this line
 	//this is a change to the requirement
+	//adding this line to see if the commit item will be linked to "The Read Function" at 6664
 }
